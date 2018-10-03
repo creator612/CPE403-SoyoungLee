5e2520db6a4309e9476dabd1cbda35176c6fdda1
creator612 <43506770+creator612@users.noreply.github.com>
2018-10-03 11:04:05 +0900

    lab3

---
+++ lab3/src_code/task2.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+uint8_t ui8PinData=2;
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+
+    while(1)
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, ui8PinData);
+        SysCtlDelay(16000000);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+        SysCtlDelay(16000000);
+        if(ui8PinData == 14) {ui8PinData=2;}
+        else if(ui8PinData == 12) {ui8PinData=14;}
+        else if(ui8PinData < 7) {ui8PinData += 6;}
+        else if(ui8PinData > 7) {ui8PinData -= 4;}
+    }
+}
